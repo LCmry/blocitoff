edcdae21e610d072212b810a07971c2f73a14556
LCmry <lcrees.2014@gmail.com>
2014-09-05 12:42:14 -0700

    Fixing routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  
   def show
-    @user = User.find(params[:id])
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
+
     @todo = Todo.new
     @todos = @user.todos
   end
