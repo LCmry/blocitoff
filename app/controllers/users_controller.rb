1c97373bea031f7c59e5f5f2d2ab8b670202c30d
LCmry <lcrees.2014@gmail.com>
2014-09-12 10:26:27 -0700

    Added user authentication before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @user = current_user
     @todo = Todo.new
     @todos = @user.todos
+    unless @user == User.find(params[:id])
+      redirect_to @user, alert: "That is not your page"
+    end
   end
 end
