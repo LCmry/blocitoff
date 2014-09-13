4eb1e79a084924c388393281c1148959c9a76eea
LCmry <lcrees.2014@gmail.com>
2014-09-13 12:37:41 -0700

    Added config for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = { host: 'http://lc-blocitoff.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://lc-blocitoff.herokuapp.com' }
 end
