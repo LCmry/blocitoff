5bd04b7ac5dcee6bf826396f73e865e75bad3fe0
LCmry <lcrees.2014@gmail.com>
2014-09-15 13:40:51 -0700

    User authorization - only current user sees own page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @todo = Todo.new
-    @todos = @user.todos   
+    @todos = @user.todos
+    unless @user == current_user
+      redirect_to user_path(current_user), alert: "That is not the page you are looking for"
+    end
   end
 end
