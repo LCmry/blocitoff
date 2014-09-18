109f564bba17d9273c7b9feffd44cac1093f55fe
LCmry <lcrees.2014@gmail.com>
2014-09-17 19:20:58 -0700

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  devise_scope :user do
-    authenticated :user do
-      root "users#show", as: :authenticated_root
-    end
-
-    unauthenticated do
-      root "welcome#index", as: :unauthenticated_root
-    end
-  end
-
   resources :users, only: [:show] do
     resources :todos, only: [:new, :create, :destroy]
   end
+
+  root to: "welcome#index"
 
 end
