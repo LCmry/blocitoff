8cb1e849e44c43cd7981062c972cba99e93ff5b6
LCmry <lcrees.2014@gmail.com>
2014-09-17 19:16:36 -0700

    Changing root routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    #if params[:id] != current_user.id
-    #  redirect_to user_path(current_user) && return
-    #end
     @user = User.find(params[:id])
-    #redirect_to(user_path(@user), :notice => 'User not found') unless @user
     @todo = Todo.new
     @todos = @user.todos
     authorize @user
